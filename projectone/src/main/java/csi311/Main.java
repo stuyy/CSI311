1337832efbf14c44b552407b3c56b043b15df00b
ansonfoong <afoong@albany.edu>
2019-01-25 17:21:22 -0500

    Check to see if the command line args is correct. Checking to see if the file specified exists.

--- projectone/src/main/java/csi311/Main.java
+++ projectone/src/main/java/csi311/Main.java
@@ -6,6 +6,30 @@ public class Main {
 	
 	public static void main(String [] args)
 	{
-		
+		// First we need to take in command line arguments, and check to see if the argument passed in is a file.
+		System.out.println(args[0]);
+		try {
+			if(args.length != 1)
+				throw new Exception("Too many arguments. Please only specify a text input file.");
+			
+			if(args.length == 1)
+			{
+				// Check if the file exists.
+				File file = new File(args[0]);
+				if(file.exists())
+				{
+					System.out.println("File Exists!");
+					FileReader fileReader = new FileReader(file);
+				}
+				else 
+				{
+					throw new FileNotFoundException("File Not Found");
+				}
+			}
+		}
+		catch(Exception ex)
+		{
+			System.out.println(ex);
+		}
 	}
 }
