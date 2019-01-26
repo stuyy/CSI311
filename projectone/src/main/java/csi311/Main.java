4bb7a178d616326c2e8409b22e177328c3f2d2e1
ansonfoong <afoong@albany.edu>
2019-01-25 19:24:11 -0500

    First take the substring of the line, getting the packageID. Save the position of the first comma

--- projectone/src/main/java/csi311/Main.java
+++ projectone/src/main/java/csi311/Main.java
@@ -7,8 +7,11 @@ public class Main {
 	public static void main(String [] args)
 	{
 		// First we need to take in command line arguments, and check to see if the argument passed in is a file.
-		System.out.println(args[0]);
 		try {
+
+			if(args.length == 0)
+				throw new Exception("No arguments provided.");
+			
 			if(args.length != 1)
 				throw new Exception("Too many arguments. Please only specify a text input file.");
 			
@@ -18,8 +21,22 @@ public class Main {
 				File file = new File(args[0]);
 				if(file.exists())
 				{
-					System.out.println("File Exists!");
 					FileReader fileReader = new FileReader(file);
+					BufferedReader bufferedReader = new BufferedReader(fileReader);
+					String line;
+					while((line = bufferedReader.readLine()) != null)
+					{
+						if(line.length() == 0)
+							continue;
+						else {
+							int firstCommaPosition = line.indexOf(",");
+							String packageID = line.substring(0, firstCommaPosition);
+							//System.out.println("The first comma appears at " + firstCommaPosition);
+							//System.out.println("Package ID: " + packageID);
+							// First let's parse the Package ID.
+							parsePackageID(packageID);
+						}
+					}
 				}
 				else 
 				{
@@ -31,5 +48,13 @@ public class Main {
 		{
 			System.out.println(ex);
 		}
+	}
+	
+	public static Boolean parsePackageID(String packageID)
+	{
+		System.out.println(packageID);
+		// If Package ID's length is not 12, return false.
+		System.out.println(packageID.length());
+		return false;
 	}
 }
